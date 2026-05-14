efa1eef8632f2627227c9a623d5972030332de66
vaarunsonawwane-ops <vaarunsonawwane@gmail.com>
2026-05-15 01:44:45 +0530

    robots.ts & sitemap.ts created
    robots.ts & sitemap.ts created

---
+++ app/sitemap.ts
@@ -0,0 +1,95 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://yoryantra.com",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/about",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/contact",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/privacy-policy",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/terms",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/disclaimer",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/sitemap",
+      lastModified: new Date(),
+    },
+
+    /* TOOLS */
+
+    {
+      url: "https://yoryantra.com/tools/json-formatter",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/base64-encoder-decoder",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/jwt-decoder",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/regex-tester",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/timestamp-converter",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/sql-formatter",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/url-encoder-decoder",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/slug-generator",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/text-case-converter",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://yoryantra.com/tools/word-counter",
+      lastModified: new Date(),
+    },
+  ];
+}
