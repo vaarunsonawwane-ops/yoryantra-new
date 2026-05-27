8dd03d35c9c74c575177abb44dcd1dbd18358d16
vaarunsonawwane-ops <vaarunsonawwane@gmail.com>
2026-05-27 18:28:45 +0530

    Google measurement id added in app/layout.tsx
    Google measurement id added in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import Header from "./components/Header";
@@ -12,9 +13,12 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://yoryantra.com"),
+
   title: "YORYANTRA",
+
   description:
     "Smart utilities for structured workflows, productivity, and modern work.",
+
   icons: {
     icon: "/YoryantraFavicon.png",
   },
@@ -28,6 +32,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white text-gray-900`}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V1ZXR0B4FM"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V1ZXR0B4FM');
+          `}
+        </Script>
+
         <Header />
 
         <main>{children}</main>
